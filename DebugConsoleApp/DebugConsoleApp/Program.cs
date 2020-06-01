85ad3e05a224b0b01c7b4de2811327607ec77ead
Ihor Dobrodii <idi@speedperform.com>
2020-06-01 17:37:53 +0300

    Updated#1

---
+++ DebugConsoleApp/DebugConsoleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DebugConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
